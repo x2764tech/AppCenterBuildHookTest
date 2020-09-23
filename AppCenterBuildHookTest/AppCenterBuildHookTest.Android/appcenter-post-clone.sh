5f2f3018e30fca1ae1addf315d704e486d550658
David Kemp <david@x2764te.ch>
2020-09-23 11:14:24 +0100

    fix(build): add build hook to create test.xml file

---
+++ AppCenterBuildHookTest/AppCenterBuildHookTest.Android/appcenter-post-clone.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+TESTXML_PATH = "${APPCENTER_SOURCE_DIRECTORY}/AppCenterBuildHookTest/AppCenterBuildHookTest.Android/Resources/values/test.xml"
+
+echo "Creating Test xml file to ${TESTXML_PATH}"
+
+echo '<?xml version="1.0" encoding="utf-8" ?><resources></resources>' > ${TESTXML_PATH}
