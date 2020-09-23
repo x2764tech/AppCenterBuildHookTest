1b96322582940eac78dfed23fb88864897989d18
David Kemp <david@x2764te.ch>
2020-09-23 09:47:41 +0100

    Add project files.

---
+++ AppCenterBuildHookTest/AppCenterBuildHookTest/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppCenterBuildHookTest.Services;
+using AppCenterBuildHookTest.Views;
+
+namespace AppCenterBuildHookTest
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppCenterBuildHookTest/AppCenterBuildHookTest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AppCenterBuildHookTest.ViewModels;
+
+namespace AppCenterBuildHookTest.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
